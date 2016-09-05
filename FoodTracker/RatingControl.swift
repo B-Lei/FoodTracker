dfec1adf140742d1d8e5bba52b427a89273c0266
Ben Lei <benjamin.g.lei@gmail.com>
2016-09-04 17:12:41 -0700

    Added button press detection

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -18,6 +18,12 @@ class RatingControl: UIView {
         // Creates a button
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.redColor()
+        
+        // Attach the ratingButtonTapped action to the button object,
+        // which is triggered on a .TouchDown event. The target is
+        // self, which is the RatingControl class, where the action is defined.
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+        
         // Adds the button to the RatingControl view
         addSubview(button)
     }
@@ -25,6 +31,12 @@ class RatingControl: UIView {
     // Tells stack view how to lay out the button
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    // =============================================================
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
 
 }
